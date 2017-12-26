a09d1d0357e4ffab0d2ae10f7d4146bb53da5c89
li <285753421@qq.com>
2017-12-26 11:59:29 +0800

    fixed router process

--- bin/app/Controller/Process.php
+++ bin/app/Controller/Process.php
@@ -12,10 +12,19 @@ class Process
 
         public function run()
         {
-                $i = 0;
-                swoole_timer_after(10000,function (){
-                        echo 'sync over';
-                });
+//                swoole_timer_after(10000,function (){
+//                        $file = rand(0,99999);
+//                        file_put_contents($file.'.txt','sync !!!'.PHP_EOL, FILE_APPEND);
+//                });
+//
+//                swoole_timer_tick(10000,function (){
+//                        $file = rand(0,99999);
+//                        file_put_contents($file.'.txt','sync !!!'.PHP_EOL, FILE_APPEND);
+//                });
+                while (true) {
+                        $file = rand(0,99999);
+                        file_put_contents($file.'.txt','sync !!!'.PHP_EOL, FILE_APPEND);
+                }
         }
 
         public function after()
